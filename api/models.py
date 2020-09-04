a9104895d86dee7d4911e41088235302675aeede
snov555 <snov555@gmail.com>
2020-09-04 15:51:01 +0300

    'add_models_comment_review'

--- api/models.py
+++ api/models.py
@@ -18,3 +18,37 @@ class User(AbstractUser):
         choices=UserRole.choices,
         default=UserRole.USER
         )
+
+
+class Review(models.Model):
+    SCORE = (
+        (1, 'One Star'),
+        (2, 'Two Stars'),
+        (3, 'Three Stars'),
+        (4, 'Four Stars'),
+        (5, 'Five Stars'),
+    )
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    created = models.DateTimeField('date published', auto_now_add=True)
+    title = models.ForeignKey('Title', on_delete=models.CASCADE, related_name='reviews')
+    score = models.PositiveSmallIntegerField(choices=SCORE)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField('date published', auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.text
