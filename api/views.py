8426961e99400da77559d1f58c5ce10c88bfdaf1
snov555 <snov555@gmail.com>
2020-09-07 19:40:42 +0300

    'pep8_check_3'

--- api/views.py
+++ api/views.py
@@ -167,4 +167,3 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
         return Comment.objects.filter(review=review)
-
